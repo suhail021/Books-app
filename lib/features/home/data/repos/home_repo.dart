d2772aa4984ca29ce625a96ec2bfaef17c4ffbf8
sohilalngm10@gmail.com <sohilalngm10@gmail.com>
2025-06-26 19:07:22 -0700

    create failure class

--- lib/features/home/data/repos/home_repo.dart
+++ lib/features/home/data/repos/home_repo.dart
@@ -1,4 +1,8 @@
+import 'package:dartz/dartz.dart';
+import 'package:google/core/errors/failures.dart';
+import 'package:google/features/home/data/models/book_model/book_model.dart';
+
 abstract class HomeRepo {
-  fetchBestSellerBooks();
-  fetchFeaturedBooks();
+  Future<Either<Failure , List<BookModel>>> fetchBestSellerBooks();
+  Future<Either<Failure , List<BookModel>>> fetchFeaturedBooks();
 }
